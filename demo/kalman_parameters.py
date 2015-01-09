730d53a685418d6d6ca7fa4a301f78c708d08e0f
brainhacker <brainhacker@lobula.(none)>
2015-01-09 11:15:18 -0800

    added demo yaml files

---
+++ demo/kalman_parameters.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+### Define kalman filter properties ########
+phi = np.matrix([  [1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                                     [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                                     [0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+                                     [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+                                     [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+                                     [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+                                     [0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+                                     [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                                     [0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+                                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]])
+H   = np.matrix([  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                     [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                                     [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                                     [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+                                     [0, 0, 0, 0, 0, 0, 0, 0, 1, 0]])
+P0  = 1*np.eye(10)
+Q   = 1*np.matrix(np.eye(10))
+R   = .01*np.matrix(np.eye(5))
+
+gamma  = None
+gammaW = None
+
+max_covariance = 50
+
+association_matrix = np.matrix([[1,1,0,0,0]], dtype=float).T
+association_matrix /= np.linalg.norm(association_matrix)
