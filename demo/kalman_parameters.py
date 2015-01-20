4a92d7447597bb3f40ff839325ab1925a5e773cd
brainhacker <brainhacker@lobula.(none)>
2015-01-20 10:35:56 -0800

    updated parameters

--- demo/kalman_parameters.py
+++ demo/kalman_parameters.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 ### Define kalman filter properties ########
-phi = np.matrix([  [1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+phi = np.matrix([                    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
                                      [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
                                      [0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
                                      [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
@@ -11,19 +11,20 @@ phi = np.matrix([  [1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
                                      [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
                                      [0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
                                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]])
-H   = np.matrix([  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+
+H   = np.matrix([                    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                                      [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
                                      [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
                                      [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
                                      [0, 0, 0, 0, 0, 0, 0, 0, 1, 0]])
 P0  = 1*np.eye(10)
-Q   = 1*np.matrix(np.eye(10))
-R   = .01*np.matrix(np.eye(5))
+Q   = 5*np.matrix(np.eye(10))
+R   = .1*np.matrix(np.eye(5))
 
 gamma  = None
 gammaW = None
 
-max_covariance = 50
+max_covariance = 1000
 
 association_matrix = np.matrix([[1,1,0,0,0]], dtype=float).T
 association_matrix /= np.linalg.norm(association_matrix)
