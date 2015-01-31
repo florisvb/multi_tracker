b1bc3b7f4e6f51c9f38b8df9a49ca0807622f7e2
Floris van Breugel <florisvb@gmail.com>
2015-01-31 14:59:59 -0800

    added some documentation and changed function names

--- multi_tracker_analysis/estimate_R_and_Q.py
+++ multi_tracker_analysis/estimate_R_and_Q.py
@@ -8,8 +8,31 @@ import matplotlib.pyplot as plt
 
 #keys = data_slicing.get_keys_of_length_greater_than(data, 30)
 
+def find_covariance_R_given_static_object_data(x):
+    '''
+    x - list or np.array - time series of measured x position of a static object (e.g. a dead fly)
+        ideally data is collected using the same image processing algorithm, arena, etc.
+    '''
+    if type(x) is list:
+        x = np.array(x)
+    x_expected = np.tile(np.mean(x), len(x))
+    x_centered = x-x_expected 
+    x_cov = np.dot(x_centered, x_centered.T)    
+    return x_cov
 
-def find_covariance_Q_for_trajectory(trajec, q=1, r=10):
+def calc_actual_covariance_for_trajectory_given_q(trajec, q=1, r=10, no_data_after_nth_frame=30):
+    '''
+    This function propagates kalman filter estimates and covariances with no new information after frame=no_data_after_nth_frame for a given Q=q*I
+    The actual covariances of the data are calculated as (cov_i = (xi-xi_expected)*(xi-xi_expected)') 
+    These can then be compared to the covariances from the kalman filter
+    
+    Returns: actual positional covariances, kalman filter positional covariances
+    
+    Strategy: run the function for various values of q, until the actual and kalman estimated covariances grow at approximately the same rate
+    
+    trajec - trajectory object from multi_tracker_analysis.read_csv_file_to_python.load_data_as_python_object_from_csv_file
+             trajectory should be minimum 2*no_data_after_nth_frame frames long, the longer the better
+    '''
 
     Q   = q*np.matrix(np.eye(kalman_parameters.phi.shape[0]))
     R   = r*np.matrix(np.eye(5))
@@ -21,31 +44,28 @@ def find_covariance_Q_for_trajectory(trajec, q=1, r=10):
                                         H=kalman_parameters.H, 
                                         Q=Q, 
                                         R=kalman_parameters.R, 
-                                        gammaW=None, 
-                                        Q_nodata=None, 
-                                        R_nodata=None)
+                                        gammaW=None,
+                                        )
 
     errors = []
     positional_covariances = []
     KF_covariances = []
     
-    for frame in range(0,30):
+    for frame in range(0,no_data_after_nth_frame):
         measurement = np.matrix([ trajec.position[frame, 0], trajec.position[frame, 1], 0, 0, 0]).T
         xhat, P, K = KF_nm.update(measurement)
         estimated_position = np.array([xhat[0,0], xhat[2,0]])
-        actual_position = np.array([trajec.position[frame, 0], trajec.position[frame,1]] )
-        print estimated_position, actual_position
+        actual_position = np.array([trajec.measured_position[frame, 0], trajec.measured_position[frame,1]] )
         error = np.linalg.norm(estimated_position - actual_position)
         errors.append(error)
         cov = error*error
         positional_covariances.append(cov)
         KF_covariances.append( np.linalg.norm(np.array([P[0,0], P[2,2]])) )
         
-    for frame in range(30,trajec.length):
+    for frame in range(no_data_after_nth_frame,trajec.length):
         xhat, P, K = KF_nm.update(None)
         estimated_position = np.array([xhat[0,0], xhat[2,0]])
-        actual_position = np.array([trajec.position[frame, 0], trajec.position[frame,1]] )
-        print estimated_position, actual_position
+        actual_position = np.array([trajec.measured_position[frame, 0], trajec.measured_position[frame,1]] )
         error = np.linalg.norm(estimated_position - actual_position)
         errors.append(error)
         cov = error*error
@@ -55,14 +75,18 @@ def find_covariance_Q_for_trajectory(trajec, q=1, r=10):
     return positional_covariances, KF_covariances
     
     
-def find_covariance_Q_for_data(data, keys, q=1, r=10):
+def calc_actual_covariance_for_all_trajecs_given_q(data, keys, q=1, r=10, no_data_after_nth_frame=30):
+    '''
+    Runs the function "calc_actual_covariance_for_trajectory_given_q" on all trajectories associated with the given keys, and plots the resulting covariances
     
+    Use "keys = multi_tracker_analysis.data_slicing.get_keys_of_length_greater_than(data, no_data_after_nth_frame*2)" to get the keys
+    '''
     p = []
     k = []
     
     for key in keys:
         trajec = data[key]
-        p_, k_ = find_covariance_Q_for_trajectory(trajec, q, r)
+        p_, k_ = find_covariance_Q_for_trajectory(trajec, q, r, no_data_after_nth_frame)
         p.append(p_)
         k.append(k_)
 
@@ -74,4 +98,14 @@ def find_covariance_Q_for_data(data, keys, q=1, r=10):
         ax.plot(k[i], color='red')
     
     plt.show()
+    
+    
+
+
+
+
+
+
+
+    
     
