fdfbe93d10a465195e155761afd287c1a700b7c3
brainhacker <brainhacker@lobula.(none)>
2015-06-04 07:56:45 -0700

    print location of kalman file

--- nodes/data_association.py
+++ nodes/data_association.py
@@ -22,6 +22,7 @@ class DataAssociator(object):
         kalman_parameter_py_file = rospy.get_param('/multi_tracker/data_association/kalman_parameters_py_file')
         home_directory = os.path.expanduser( rospy.get_param('/multi_tracker/home_directory') )
         kalman_parameter_py_file = os.path.join(home_directory, kalman_parameter_py_file)
+        print 'Kalman py file: ', kalman_parameter_py_file
         
         self.kalman_parameters = imp.load_source('kalman_parameters', kalman_parameter_py_file)
         self.association_matrix = self.kalman_parameters.association_matrix
