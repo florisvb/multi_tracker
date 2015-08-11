f73b2afd342c340b9a6d3fff6ab00fddc739ea4e
Floris van Breugel <florisvb@gmail.com>
2015-08-11 11:11:52 -0700

    new h5 format is much faster

--- nodes/save_data_to_hdf5.py
+++ nodes/save_data_to_hdf5.py
@@ -29,11 +29,12 @@ class DataListener:
         self.lockParams = threading.Lock()
         self.lockBuffer = threading.Lock()
         
-        self.chunk_size = 500
+        self.chunk_size = 5000
         self.hdf5 = h5py.File(filename, 'w')
         self.hdf5.attrs.create("info", info)
         
-        self.data_to_save = [   'header.stamp.secs',
+        self.data_to_save = [   'objid',
+                                'header.stamp.secs',
                                 'header.stamp.nsecs', 
                                 'header.frame_id', 
                                 'position.x', 
@@ -48,7 +49,8 @@ class DataListener:
                                 'measurement.x',
                                 'measurement.y',
                                 ]
-        self.data_format = {    'header.stamp.secs': int,
+        self.data_format = {    'objid': int,
+                                'header.stamp.secs': int,
                                 'header.stamp.nsecs': int, 
                                 'header.frame_id': int, 
                                 'position.x': float, 
@@ -65,55 +67,43 @@ class DataListener:
                             }
                             
         self.dtype = [(data,self.data_format[data]) for data in self.data_to_save]
-        rospy.init_node('save_data_to_hdf5')
+        rospy.init_node('save_data_to_hdf5_unsorted')
         
-    def create_hdf5_object(self, objid, frame_camera):
-        self.hdf5.create_dataset(objid, (self.chunk_size, 1), maxshape=(None,1), dtype=self.dtype)
-        self.hdf5[objid].attrs.create('objid', objid)
-        self.hdf5[objid].attrs.create('current_frame', 0)
-        self.hdf5[objid].attrs.create('first_camera_frame', frame_camera)
-        self.hdf5[objid].attrs.create('length', self.chunk_size)
+        self.hdf5.create_dataset('data', (self.chunk_size, 1), maxshape=(None,1), dtype=self.dtype)
+        self.hdf5['data'].attrs.create('current_frame', 0)
+        self.hdf5['data'].attrs.create('line', 0)
+        self.hdf5['data'].attrs.create('length', self.chunk_size)
         
-    def add_chunk(self, obj):
-        length = obj.attrs.get('length')
+    def add_chunk(self):
+        length = self.hdf5['data'].attrs.get('length')
         new_length = length + self.chunk_size
-        obj.resize(new_length, axis=0)
-        obj.attrs.modify('length', new_length)
+        self.hdf5['data'].resize(new_length, axis=0)
+        self.hdf5['data'].attrs.modify('length', new_length)
             
-    def save_data(self, obj, tracked_object, frame):
-        obj[frame] = np.array([(    tracked_object.header.stamp.secs,
-                                    tracked_object.header.stamp.nsecs,
-                                    tracked_object.header.frame_id,
-                                    tracked_object.position.x, tracked_object.position.y, tracked_object.position.z,
-                                    tracked_object.velocity.x, tracked_object.velocity.y, tracked_object.velocity.z,
-                                    tracked_object.angle,
-                                    tracked_object.size,
-                                    tracked_object.covariance,
-                                    tracked_object.measurement.x, tracked_object.measurement.y,
-                               )], dtype=self.dtype)
+    def save_data(self, tracked_object):
+        newline = self.hdf5['data'].attrs.get('line') + 1
+        self.hdf5['data'].attrs.modify('line', newline)
+        if newline >= self.hdf5['data'].attrs.get('length')-50:
+                self.add_chunk()
+        self.hdf5['data'][newline] = np.array([(     tracked_object.objid,
+                                                        tracked_object.header.stamp.secs,
+                                                        tracked_object.header.stamp.nsecs,
+                                                        tracked_object.header.frame_id,
+                                                        tracked_object.position.x, tracked_object.position.y, tracked_object.position.z,
+                                                        tracked_object.velocity.x, tracked_object.velocity.y, tracked_object.velocity.z,
+                                                        tracked_object.angle,
+                                                        tracked_object.size,
+                                                        tracked_object.covariance,
+                                                        tracked_object.measurement.x, tracked_object.measurement.y,
+                                                   )], dtype=self.dtype)
+                                                   
     def tracked_object_callback(self, tracked_objects):
         with self.lockBuffer:
             self.buffer.append(tracked_objects)
         
     def process_buffer(self, tracked_objects):
         for tracked_object in tracked_objects.tracked_objects:
-            
-            frame_camera = int(tracked_object.header.frame_id)
-            
-            objid = str(tracked_object.objid)
-            if objid not in self.hdf5:
-                self.create_hdf5_object(objid, frame_camera)
-            
-            obj = self.hdf5[objid]
-            frame_obj = frame_camera - obj.attrs.get('first_camera_frame')
-            
-            if frame_obj >= obj.attrs.get('length'):
-                self.add_chunk(obj)
-            
-            self.save_data(obj, tracked_object, frame_obj)
-            obj.attrs.modify('current_frame',  frame_obj+1)
-            
-            #print objid, obj.attrs.get('current_frame'), obj.attrs.get('length'), obj['position'].shape
+            self.save_data(tracked_object)
             
     def main(self):
         atexit.register(self.stop_saving_data)
