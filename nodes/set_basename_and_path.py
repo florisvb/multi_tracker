7f5ee094bc11dc13c373a5ba980ca57d72b1d547
Floris van Breugel <florisvb@gmail.com>
2015-09-04 09:23:55 -0700

    hopefully can use this to sync names across nodes more effectively

---
+++ nodes/set_basename_and_path.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from optparse import OptionParser
+import roslib
+import rospy
+import rosparam
+import time
+
+#####################################################################################################
+    
+if __name__ == '__main__':
+    parser = OptionParser()
+    parser.add_option("--nodenum", type="str", dest="nodenum", default='1',
+                        help="node number, for example, if running multiple tracker instances on one computer")
+    (options, args) = parser.parse_args()
+    
+    experiment_basename = time.strftime("%Y%m%d_%H%M%S_N" + nodenum, time.localtime())
+    rospy.set_param('/multi_tracker/' + nodenum + '/experiment_basename', experiment_basename)
