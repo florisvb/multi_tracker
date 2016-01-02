cac31aa75353a0d05504b24a8f5e5864a1d7d8af
Floris van Breugel <florisvb@gmail.com>
2016-01-02 13:54:04 -0800

    fit ellipse added

--- multi_tracker_analysis/image_analysis.py
+++ multi_tracker_analysis/image_analysis.py
@@ -84,6 +84,55 @@ class ClickCircle(object):
         while (cv2.waitKey(30) != 27):
             self.draw()
         cv.destroyAllWindows();
+        
+##############################################################################################
+# Ellipse
+
+def estimate_ellipse_from_data_points(x_m, y_m):
+    points = []
+    for i in range(len(x_m)):
+        points.append((x_m[i], y_m[i]))
+    ellipse = cv2.fitEllipse(np.array(points))
+    return ellipse
+    
+class ClickEllipse(object):
+    def __init__(self, filename):
+        self.image = cv2.imread(filename)
+        
+        self.display_name = "Display"
+        cv2.namedWindow(self.display_name)
+        
+        cv2.setMouseCallback(self.display_name, self.on_mouse_click)
+        
+        self.circle_points_x = []
+        self.circle_points_y = []
+        self.circle_fit = None
+        
+    def on_mouse_click(self, event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONUP:
+            self.circle_points_x.append(x)
+            self.circle_points_y.append(y)
+            
+        if len(self.circle_points_x) >= 5:
+            ellipse = estimate_ellipse_from_data_points(self.circle_points_x, self.circle_points_y)
+            self.circle_fit = ellipse
+            
+    def draw(self):
+        canvas = copy.copy(self.image)
+        for i in range(len(self.circle_points_x)):
+            cv2.circle(canvas, (self.circle_points_x[i], self.circle_points_y[i]), 2, [0,0,255], 2)
+        if self.circle_fit is not None:
+            print self.circle_fit
+            print (int(self.circle_fit[0][0]), int(self.circle_fit[0][1]))
+            cv2.ellipse(canvas, (int(self.circle_fit[0][0]), int(self.circle_fit[0][1])), (int(self.circle_fit[1][0]/2.), int(self.circle_fit[1][1]/2.)), int(self.circle_fit[2]), 0, 360, (0,255,0), 2 )
+        
+        cv2.imshow("Display", canvas)
+        #cv2.waitKey(1)
+        
+    def run(self):
+        while (cv2.waitKey(30) != 27):
+            self.draw()
+        cv.destroyAllWindows();
 
 ##############################################################################################
 # Pixels
@@ -129,6 +178,8 @@ if __name__ == '__main__':
         imageGUI = ClickPixels(options.filename)
     elif options.analysis == 'circle':
         imageGUI = ClickCircle(options.filename)
+    elif options.analysis == 'ellipse':
+        imageGUI = ClickEllipse(options.filename)
         
     imageGUI.run()
             
