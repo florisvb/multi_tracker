bec940df02cbc2f98fc5ba6ee90e82ee33cb2e66
Floris van Breugel <florisvb@gmail.com>
2016-04-08 16:12:39 -0700

    added support for auto file creation

--- nodes/delta_video_to_pandas.py
+++ nodes/delta_video_to_pandas.py
@@ -29,16 +29,28 @@ import multi_tracker_analysis.dvbag_to_pandas_reader as dvbag_to_pandas_reader
 
 # The main tracking class, a ROS node
 class DeCompressor:
-    def __init__(self, topic_in, saveto=''):
+    def __init__(self, nodenum, saveto=''):
                         
         # initialize the node
         rospy.init_node('delta_to_pandas')
         self.nodename = rospy.get_name().rstrip('/')
         
         # Publishers - publish contours
-        self.subDeltaVid = rospy.Subscriber(topic_in, DeltaVid, self.delta_image_callback, queue_size=300)
+        topic = '/multi_tracker/' + str(nodenum) + '/delta_video'
+        self.subDeltaVid = rospy.Subscriber(topic, DeltaVid, self.delta_image_callback, queue_size=300)
         
-        self.saveto = saveto
+        if len(saveto) < 1:
+            experiment_basename = rospy.get_param('/multi_tracker/' + nodenum + '/experiment_basename', 'none')
+            if experiment_basename == 'none':
+                experiment_basename = time.strftime("%Y%m%d_%H%M%S_N" + nodenum, time.localtime())
+           
+            filename = experiment_basename + '_delta_video_as_pandas.hdf'
+            home_directory = os.path.expanduser( rospy.get_param('/multi_tracker/' + nodenum + '/data_directory') )
+            filename = os.path.join(home_directory, filename)
+            self.saveto = filename
+        else:
+            self.saveto = saveto
+        
         self.dvbag = dvbag_to_pandas_reader.DVBag2PandasReader(saveto=saveto)
         
     def delta_image_callback(self, delta_vid):
@@ -51,13 +63,13 @@ class DeCompressor:
     
 if __name__ == '__main__':
     parser = OptionParser()
-    parser.add_option("--in", type="str", dest="input", default='/multi_tracker/delta_video',
-                        help="input topic name")
     parser.add_option("--saveto", type="str", dest="saveto", default='',
                         help="filename where to save video, default is none")
+    parser.add_option("--nodenum", type="int", dest="nodenum", default=1,
+                        help="nodenumber")
     
     (options, args) = parser.parse_args()
     
-    decompressor = DeCompressor(options.input, options.saveto)
+    decompressor = DeCompressor(options.nodenum, options.saveto)
 
     decompressor.Main()
