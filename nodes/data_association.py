a0b4001b1cd3a052d23c5b39abb4cbd7820bf4b0
Floris van Breugel <florisvb@gmail.com>
2016-05-31 12:21:08 -0700

    made kalman file searching more robust

--- nodes/data_association.py
+++ nodes/data_association.py
@@ -24,12 +24,17 @@ import threading
 class DataAssociator(object):
     def __init__(self, nodenum):
         kalman_parameter_py_file = os.path.expanduser( rospy.get_param('/multi_tracker/' + nodenum + '/data_association/kalman_parameters_py_file') )
-        home_directory = os.path.expanduser( rospy.get_param('/multi_tracker/' + nodenum + '/home_directory') )
-        print 'Kalman py file: ', kalman_parameter_py_file
-        
         self.lockBuffer = threading.Lock()
         
-        self.kalman_parameters = imp.load_source('kalman_parameters', kalman_parameter_py_file)
+        try:
+            self.kalman_parameters = imp.load_source('kalman_parameters', kalman_parameter_py_file)
+            print 'Kalman py file: ', kalman_parameter_py_file
+        except: # look in home directory for kalman parameter file
+            home_directory = os.path.expanduser( rospy.get_param('/multi_tracker/' + nodenum + '/home_directory') )
+            kalman_parameter_py_file = os.path.join(home_directory, kalman_parameter_py_file)
+            self.kalman_parameters = imp.load_source('kalman_parameters', kalman_parameter_py_file)
+            print 'Kalman py file: ', kalman_parameter_py_file
+            
         self.association_matrix = self.kalman_parameters.association_matrix
         self.association_matrix /= np.linalg.norm(self.association_matrix)
         self.max_covariance = self.kalman_parameters.max_covariance
