bb11250799282f6dfb870189cd5790635d02ba7b
Floris van Breugel <florisvb@gmail.com>
2016-05-31 12:46:54 -0700

    fixed example launch stuff

--- demo/demo_1/src/kalman_parameters.py
+++ demo/demo_1/src/kalman_parameters.py
@@ -18,13 +18,13 @@ H   = np.matrix([                    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                                      [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
                                      [0, 0, 0, 0, 0, 0, 0, 0, 1, 0]])
 P0  = 10*np.eye(10)
-Q   = .5*np.matrix(np.eye(10))
-R   = 20*np.matrix(np.eye(5))
+Q   = .2*np.matrix(np.eye(10))
+R   = 50*np.matrix(np.eye(5))
 
 gamma  = None
 gammaW = None
 
-max_covariance = 50
+max_covariance = 30
 max_velocity = 10
 
 association_matrix = np.matrix([[1,1,0,0,0]], dtype=float).T
