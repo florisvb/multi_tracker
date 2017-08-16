7a4bc88512b7ef22051316a3635c3304abe58632
florisvb <florisvb@gmail.com>
2017-08-16 11:41:56 -0700

    improvements

--- nodes/trigger_gphoto2_camera.py
+++ nodes/trigger_gphoto2_camera.py
@@ -4,25 +4,31 @@
 from optparse import OptionParser
 import roslib
 import rospy
-
-from std_msgs.msg import Bool
-import gphoto2 as gp
 import os
+import time
+
+from std_msgs.msg import Float32MultiArray
+
+'''
+sudo apt-get install libgphoto2-dev
+sudo pip install -v gphoto2 (takes a while, be patient)
+'''
+import gphoto2 as gp
             
 # The main tracking class, a ROS node
 class GPhotoCamera:
-    def __init__(self, nodenum):
-        # default parameters (parameter server overides them)
-        '''
-        self.params = { 'min_persistence'   : 10,
-                        }
-        for parameter, value in self.params.items():
-            try:
-                p = '/multi_tracker/' + nodenum + '/prefobj/' + parameter
-                self.params[parameter] = rospy.get_param(p)
-            except:
-                pass
-        '''
+    def __init__(self, nodenum, topic):
+        # where to save photos
+        home_directory = os.path.expanduser( rospy.get_param('/multi_tracker/' + nodenum + '/data_directory') )
+        experiment_basename = rospy.get_param('/multi_tracker/' + nodenum + '/experiment_basename', 'none')
+        if experiment_basename == 'none':
+            experiment_basename = time.strftime("%Y%m%d_%H%M%S_N" + nodenum, time.localtime())
+        directory_name = experiment_basename + '_gphoto2'
+        self.destination = os.path.join(home_directory, directory_name)
+        if os.path.exists(self.destination):
+            pass
+        else:
+            os.mkdir(self.destination)
 
         # initialize the node
         rospy.init_node('gphoto2_' + nodenum)
@@ -33,11 +39,39 @@ class GPhotoCamera:
         self.context = gp.gp_context_new()
         self.camera = gp.check_result(gp.gp_camera_new())
         gp.check_result(gp.gp_camera_init(self.camera, self.context))
+        self.synchronize_camera_timestamp()
         
-        self.subTrackedObjects = rospy.Subscriber('/multi_tracker/' + nodenum + '/gphoto', Bool, self.gphoto_callback)
+        self.subTrackedObjects = rospy.Subscriber('/multi_tracker/' + nodenum + '/' + topic, Float32MultiArray, self.gphoto_callback)
         
-    def gphoto_callback(self, msg):
+    def synchronize_camera_timestamp(self):
+        def set_datetime(config):
+            OK, date_config = gp.gp_widget_get_child_by_name(config, 'datetime')
+            if OK >= gp.GP_OK:
+                widget_type = gp.check_result(gp.gp_widget_get_type(date_config))
+                if widget_type == gp.GP_WIDGET_DATE:
+                    now = int(time.time())
+                    gp.check_result(gp.gp_widget_set_value(date_config, now))
+                else:
+                    now = time.strftime('%Y-%m-%d %H:%M:%S')
+                    gp.check_result(gp.gp_widget_set_value(date_config, now))
+                return True
+            return False
 
+        # get configuration tree
+        config = gp.check_result(gp.gp_camera_get_config(self.camera, self.context))
+        # find the date/time setting config item and set it
+        if set_datetime(config):
+            # apply the changed config
+            gp.check_result(gp.gp_camera_set_config(self.camera, config, self.context))
+        else:
+            print('Could not set date & time')
+        # clean up
+        gp.check_result(gp.gp_camera_exit(self.camera, self.context))
+        return 0
+
+
+
+    def gphoto_callback(self, msg):
         
         print('Capturing image')
 
@@ -45,7 +79,10 @@ class GPhotoCamera:
             self.camera, gp.GP_CAPTURE_IMAGE, self.context))
         print('Camera file path: {0}/{1}'.format(file_path.folder, file_path.name))
 
-        target = os.path.join('/home/caveman', file_path.name)
+        t = rospy.Time.now()
+        time_base = time.strftime("%Y%m%d_%H%M%S_N" + self.nodenum, time.localtime())
+        name = time_base + '_' + str(t.secs) + '_' + str(t.nsecs) + '.jpg'
+        target = os.path.join(self.destination, name)
 
         print('Copying image to', target)
         camera_file = gp.check_result(gp.gp_camera_file_get(
@@ -66,7 +103,9 @@ if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option("--nodenum", type="str", dest="nodenum", default='1',
                         help="node number, for example, if running multiple tracker instances on one computer")
+    parser.add_option("--topic", type="str", dest="topic", default='prefobj',
+                        help="topic name (will be appended to /multi_tracker/N/). Defaults to the prefobj topic.")
     (options, args) = parser.parse_args()
     
-    gphotocamera = GPhotoCamera(options.nodenum)
+    gphotocamera = GPhotoCamera(options.nodenum, options.topic)
     gphotocamera.Main()
